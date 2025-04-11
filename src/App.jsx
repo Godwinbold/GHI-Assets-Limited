4477f140caaf28db9794bb4a81c2b4936b6525b7
Godwinbold <gordonblaze5@gmail.com>
2025-04-11 11:39:56 +0100

    Company profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import QRPage from "./pages/QRPage";
+
 import CompanyPage from "./pages/companypage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<QRPage />} />
-        <Route path="/company" element={<CompanyPage />} />
+        <Route path="/" element={<CompanyPage />} />
       </Routes>
     </Router>
   );
